779edf9a6b2185103e2853b04e14cfe2b4b75055
Raul Fandiño <raul.fandino@endava.com>
2018-07-17 20:15:49 -0500

    first commit for exercise 1

---
+++ README.md
@@ -0,0 +1,1 @@
+"# git_intership_2018_2" 
---
+++ git_exercise_1.rb
@@ -0,0 +1,22 @@
+require 'rubygems'
+require 'watir'
+require 'pry'
+require 'filter'
+
+browser = Watir::Browser.new :chrome
+browser.goto 'http://todomvc.com/examples/react/#/'
+
+
+Dir.entries(".").filter(/txt/).each{ |file_name|
+	info = File.read(file_name)
+	id = info.split("\n")[0]
+	name = info.split("\n")[1]
+	last_name = info.split("\n")[2]
+	discipline = info.split("\n")[3]
+	hobbies = info.split("\n")[4].split(",")
+
+	fail "#{name} you should have exactly 3 hobbies for this exercise :)" unless hobbies.size==3
+
+	browser.text_field(:class => 'new-todo').set("Go with #{name.split(" ")[0]} to #{hobbies.sample.strip}")
+	browser.text_field(:class => 'new-todo').send_keys :enter
+}
